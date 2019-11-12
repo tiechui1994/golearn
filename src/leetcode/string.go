b040f3bd978f574ee140cc6785d4c69c95fe2bb3
tiechui1994 <2904952429@qq.com>
2019-11-12 17:13:15 +0800

    add longest paildom

--- src/leetcode/string.go
+++ src/leetcode/string.go
@@ -49,3 +49,56 @@ func stringIP(s string) []string {
 	RestoreIP(s, 4, 0, "", &res)
 	return res
 }
+
+// 最长回文子串: 中心扩展算法
+// O(n^2)
+
+func longPalindromeI(str string) string {
+	expandCenter := func(left, right int) int {
+		L := left
+		R := right
+		for L >= 0 && R < len(str) && str[L] == str[R] {
+			L--
+			R++
+		}
+
+		return R - L - 1
+	}
+
+	var start, end = 0, 0
+	for i := 0; i < len(str); i++ {
+		L1 := expandCenter(i, i)
+		L2 := expandCenter(i, i+1)
+		length := L1 - L2
+		if length < 0 {
+			length = -length
+		}
+		if length > end-start {
+			start = i - (length-1)/2
+			end = i + length/2
+		}
+	}
+
+	return str[start : end+1]
+}
+
+// O(n^2)
+func longPalindromeII(str string) string {
+	var n = len(str)
+	var result [n][n]bool
+	var start, end int
+	max := 0
+	for i := 0; i < n; i++ {
+		for j := i; j >= 0; j-- {
+			if str[i] == str[j] && (i-j < 2 || result[j+1][i-1]) {
+				result[j][i] = true
+				if max < i-j+1 {
+					max = i - j + 1
+					start = i
+					end = j
+				}
+			}
+		}
+	}
+	return str[start : end+1]
+}
