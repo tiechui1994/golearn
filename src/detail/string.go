9c9d3ebdf8775804b1684fea7d4f0c5418a1e9dd
tiechui1994 <2904952429@qq.com>
2019-12-17 11:21:51 +0800

    add substr search alg

--- src/detail/string.go
+++ src/detail/string.go
@@ -43,3 +43,63 @@ func Convert() {
 
 	fmt.Println(int('✓'), int('✔'), int('✕'))
 }
+
+// primeRK is the prime base used in Rabin-Karp algorithm.
+const primeRK = 16777619
+
+// hashStr returns the hash and the appropriate multiplicative
+// factor for use in Rabin-Karp algorithm.
+func hashStr(sep string) (uint32, uint32) {
+	// hash 算法
+	hash := uint32(0)
+	for i := 0; i < len(sep); i++ {
+		hash = hash*primeRK + uint32(sep[i])
+	}
+
+	// Rabin 当中的乘数
+	var pow, sq uint32 = 1, primeRK
+	for i := len(sep); i > 0; i >>= 1 {
+		if i&1 != 0 {
+			pow *= sq
+		}
+		sq *= sq
+	}
+	return hash, pow
+}
+
+/**
+核心: 利用了hash算法的递归计算效应
+**/
+
+func IndexRabinKarp(s, substr string) int {
+	if len(s) < len(substr) {
+		return -1
+	}
+	// Rabin-Karp search
+	hashss, pow := hashStr(substr)
+
+	// 快速比较
+	n := len(substr)
+	var h uint32
+	for i := 0; i < n; i++ {
+		h = h*primeRK + uint32(s[i])
+	}
+	if h == hashss && s[:n] == substr {
+		return 0
+	}
+
+	// 慢比较
+	for i := n; i < len(s); {
+		h = h*primeRK + uint32(s[i])
+		h -= pow * uint32(s[i-n])
+		i++
+		if h == hashss && s[i-n:i] == substr {
+			return i - n
+		}
+	}
+	return -1
+}
+
+func main() {
+	println(IndexRabinKarp("Rabin–Karp string search algorithm: Rabin-Karp", "string"))
+}
