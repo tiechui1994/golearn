998a21ec100ae31a0125268c156640939b69b198
tiechui1994 <2904952429@qq.com>
2020-03-07 10:28:36 +0800

    add kmp alg

--- src/algorithm/string_search.go
+++ src/algorithm/string_search.go
@@ -30,6 +30,38 @@ func BruteForce(main string, sub string) int {
 }
 
 // KMP算法
+//
+// BF 算法效率低在于每轮主串从pos位置开始比较过程中, 若字符不相等, 主串会回退到 pos+1 位置, 然后与匹配串第一个位置字符开始比较.
+//
+// KMP 算法首先算出一个next数组, 匹配串每轮匹配在 j 位置失配时, 匹配串向右滑动的距离为 j - next[j].
+// next 数组的计算规则如下. 其中 j 为匹配串索引：
+//
+// 1. j=0 时, next[j] = -1;
+//
+// 2. j>0, 匹配串 0 至 j-1 位置的子串前缀和后缀数组中最长相同元素的长度为maxL, 则next[j] = maxL.
+// 特殊的, 当j=1时,前缀和后缀都为空, 所以maxL=0, next[1] = 0:
+//
 func KMP(main string, sub string) int {
+
+	getNext := func(sub string) []int {
+		length := len(sub)
+		next := make([]int, length)
+		next[0] = -1
+
+		// 保存前缀和后缀数组最长相同子串
+		k := 0
+		for j := 1; j < length-1; j++ {
+			for (k > 0) && (sub[j-1] != sub[k]) {
+				k = next[j-1]
+			}
+			if (j > 1) && (sub[j-1] == sub[k]) {
+				k++
+			}
+			next[j] = k
+		}
+
+		return next
+	}
+
 	return -1
 }
