94ca3bea54fada3fcd79a08cc5e761a015b94660
tiechui1994 <2904952429@qq.com>
2020-03-12 18:03:54 +0800

    update udp comment

---
+++ src/udp/client.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"encoding/hex"
+	"log"
+	"net"
+	"time"
+)
+
+/**
+// param network:
+// The network must be a UDP network name; "udp", "udp4" (IPv4-only), "udp6" (IPv6-only)
+//
+// param raddr:
+// If laddr is nil, a local address is automatically chosen.
+// If the IP field of raddr is nil or an unspecified IP address, the local system is assumed.
+//
+
+//
+// DialUDP acts like Dial for UDP networks.
+//
+DialUDP(network string, laddr, raddr *UDPAddr) (*UDPConn, error)
+
+
+//
+// ListenUDP acts like ListenPacket for UDP networks.
+//
+func ListenUDP(network string, laddr *UDPAddr) (*UDPConn, error)
+
+
+
+//
+// ListenMulticastUDP acts like ListenPacket for UDP networks but takes a group address on a specific network interface.
+//
+// ListenMulticastUDP listens on all available IP addresses of the local system including the group, multicast IP address.
+//
+// If ifi is nil, ListenMulticastUDP uses the system-assigned multicast interface, although this is not recommended because
+// the assignment depends on platforms and sometimes it might require routing configuration.
+//
+// If the Port field of gaddr is 0, a port number is automatically chosen.
+//
+// There are golang.org/x/net/ipv4 and golang.org/x/net/ipv6 packages for general purpose uses.
+//
+func ListenMulticastUDP(network string, ifi *Interface, gaddr *UDPAddr) (*UDPConn, error)
+
+*/
+
+// 广播的client
+func BroadCastClient() {
+	ip := net.ParseIP("255.255.255.255")
+	srcAddr := &net.UDPAddr{IP: net.IPv4zero, Port: 0}
+	dstAddr := &net.UDPAddr{IP: ip, Port: 80}
+	conn, err := net.ListenUDP("udp", srcAddr)
+	if err != nil {
+		log.Println(err)
+	}
+
+	buf := make([]byte, 10240)
+	for i := 0; i < 10; i++ {
+		origin := "5aa5aa555aa5aa55000000000000000000000000000000000000000000000000b1c20000000006000000000000000000"
+		data, _ := hex.DecodeString(origin)
+		n, err := conn.WriteToUDP(data, dstAddr)
+		if err != nil {
+			log.Println(err)
+		}
+
+		n, addr, err := conn.ReadFrom(buf)
+		if err != nil {
+			log.Println(err)
+		}
+		log.Printf("Read [%s] from <%v>. Length:%v", hex.EncodeToString(buf[:n])[:10], addr.String(), n)
+		time.Sleep(3 * time.Second)
+	}
+}
+
+// 多播的client
+func MulticastClient() {
+	ip := net.ParseIP("224.0.0.250")
+	srcAddr := &net.UDPAddr{IP: net.IPv4zero, Port: 0}
+	dstAddr := &net.UDPAddr{IP: ip, Port: 1024}
+	conn, err := net.DialUDP("udp", srcAddr, dstAddr)
+	if err != nil {
+		log.Println("DialUDP:", err)
+		return
+	}
+	defer conn.Close()
+
+	var buf = make([]byte, 1024)
+	_, err = conn.Write([]byte("hello"))
+	if err != nil {
+		log.Println("WriteToUDP:", err)
+		return
+	}
+
+	n, addr, err := conn.ReadFromUDP(buf)
+	if err != nil {
+		log.Println("ReadFromUDP:", err)
+		return
+	}
+	log.Printf("read from <%v> data [%v]", addr.String(), n)
+}
+
+
+func main() {
+	MulticastClient()
+}
---
+++ src/udp/server.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"golang.org/x/net/ipv4"
+)
+
+// 文档: https://colobu.com/2016/10/19/Go-UDP-Programming/
+
+// 多播
+// 首先找到要进行多播所使用的网卡, 然后监听本机合适的地址和服务端口.
+// 将这个应用加入到多播组中, 它就可以从组中监听包信息, 当然你还可以对包传输进行更多的控制设置.
+// 应用收到包后还可以检查包是否来自这个组的包.
+func MulticastCommon() {
+	//1. 获取一个interface
+	eth, err := net.InterfaceByName("enp0s31f6")
+	if err != nil {
+		log.Printf("InterfaceByName: %v", err)
+		return
+	}
+
+	group := net.IPv4(224, 0, 0, 250)
+
+	//2. bind本地地址
+	c, err := net.ListenPacket("udp", "0.0.0.0:1024")
+	if err != nil {
+		log.Printf("ListenPacket:%v", err)
+		return
+	}
+	defer c.Close()
+
+	//3.
+	p := ipv4.NewPacketConn(c)
+	if err := p.JoinGroup(eth, &net.UDPAddr{IP: group}); err != nil {
+		log.Printf("JoinGroup:%v", err)
+		return
+	}
+
+	//4. 更多控制
+	if err := p.SetControlMessage(ipv4.FlagInterface, true); err != nil {
+		log.Printf("SetControl: %v", err)
+	}
+
+
+	//5. 接收消息
+	buf := make([]byte, 1500)
+	for {
+		n, cm, src, err := p.ReadFrom(buf)
+		if err != nil {
+			log.Printf("Read:%v", err)
+			continue
+		}
+
+		if cm.Dst.IsMulticast() {
+			if cm.Dst.Equal(group) {
+				log.Printf("received [%s] from <%s>", buf[:n], src.String())
+				n, err = p.WriteTo([]byte("BroadCast"), cm, src)
+				if err != nil {
+					log.Printf("Write:%v", err)
+					continue
+				}
+				log.Printf("send to <%s> success", src)
+			} else {
+				log.Printf("Unknown group")
+				continue
+			}
+		}
+	}
+}
+
+func MulticastSimple() {
+	addr, err := net.ResolveUDPAddr("udp", "224.0.0.250:1024")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// 如果第二参数为nil, 它会使用系统指定多播接口,但是不推荐这样使用
+	listener, err := net.ListenMulticastUDP("udp", nil, addr)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	log.Printf("Local: <%s> \n", listener.LocalAddr().String())
+
+	data := make([]byte, 1024)
+	for {
+		n, remoteAddr, err := listener.ReadFromUDP(data)
+		if err != nil {
+			log.Printf("error during read: %s", err)
+			continue
+		}
+
+		log.Printf("<%s> %s\n", remoteAddr, data[:n])
+
+		_, err = listener.WriteToUDP(data[:n], remoteAddr)
+		if err != nil {
+			log.Printf("error during write: %s", err)
+			continue
+		}
+	}
+}
+
+// 广播
+func BroadCast() {
+	listener, err := net.ListenUDP("udp", &net.UDPAddr{IP: net.IPv4zero, Port: 9981})
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	log.Printf("Local: <%s> \n", listener.LocalAddr().String())
+	data := make([]byte, 1024)
+	for {
+		n, remoteAddr, err := listener.ReadFromUDP(data)
+		if err != nil {
+			log.Printf("error during read: %s", err)
+		}
+		log.Printf("<%s> %s\n", remoteAddr, data[:n])
+		_, err = listener.WriteToUDP([]byte("world"), remoteAddr)
+		if err != nil {
+			log.Printf(err.Error())
+		}
+	}
+}
+
+func main() {
+	MulticastCommon()
+}
