c9150227e8d98dff1577ff0eb30d6c84927c74e0
tiechui1994 <2904951429@qq.com>
2020-03-15 15:42:44 +0800

    add origin udp

--- src/udp/broad.go
+++ src/udp/broad.go
@@ -1,130 +1,97 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net"
 	"os"
-	"strconv"
-	"strings"
 	"syscall"
 
 	"golang.org/x/net/ipv4"
 )
 
+func init() {
+	log.SetFlags(log.Lshortfile | log.Ldate | log.Ltime)
+}
 func main() {
-	//prog := "mcast_listener"
-
-	//if len(os.Args) != 5 {
-	//	fmt.Printf("usage:   %s interface protocol group     address:port\n", prog)
-	//	fmt.Printf("example: %s eth2      udp      224.0.0.9 0.0.0.0:2000\n", prog)
-	//	return
-	//}
-
-	//ifname := os.Args[1]
-	//proto := os.Args[2]
-	//group := os.Args[3]
-	//addrPort := os.Args[4]
-
-	ifname := "enp0s31f6"
-	proto := "udp"
-	group := "224.0.0.9"
-	addrPort := "0.0.0.0:2000"
-
-	mcastRead(ifname, proto, group, addrPort)
+	mcastRead()
 }
 
-func mcastRead(ifname, proto, group, addrPort string) {
-	addr, port := splitHostPort(addrPort)
-	p, err1 := strconv.Atoi(port)
-	if err1 != nil {
-		log.Fatal(err1)
+func mcastRead() {
+	group := net.IPv4(224, 0, 0, 9)
+
+	ifi, err := net.InterfaceByName("wlp2s0")
+	if err != nil {
+		log.Fatal("InterfaceByName:", err)
 	}
 
-	a := net.ParseIP(addr)
-	if a == nil {
-		log.Fatal(fmt.Errorf("bad address: '%s'", addr))
+	pc, err := mcastOpen(net.IPv4zero, 2000, "wlp2s0")
+	if err != nil {
+		log.Fatal("mcastOpen:", err)
 	}
 
-	g := net.ParseIP(group)
-	if g == nil {
-		log.Fatal(fmt.Errorf("bad group: '%s'", group))
+	if err := pc.JoinGroup(ifi, &net.UDPAddr{IP: group}); err != nil {
+		log.Fatal("JoinGroup:", err)
 	}
 
-	ifi, err2 := net.InterfaceByName(ifname)
-	if err2 != nil {
-		log.Fatal(err2)
+	if err := pc.SetControlMessage(ipv4.FlagTTL|ipv4.FlagSrc|ipv4.FlagDst|ipv4.FlagInterface, true); err != nil {
+		log.Fatal("SetControlMessage:", err)
 	}
 
-	c, err3 := mcastOpen(a, p, ifname)
-	if err3 != nil {
-		log.Fatal(err3)
-	}
+	readLoop(pc)
 
-	if err := c.JoinGroup(ifi, &net.UDPAddr{IP: g}); err != nil {
-		log.Fatal(err)
-	}
-
-	if err := c.SetControlMessage(ipv4.FlagTTL|ipv4.FlagSrc|ipv4.FlagDst|ipv4.FlagInterface, true); err != nil {
-		log.Fatal(err)
-	}
-
-	readLoop(c)
-
-	c.Close()
-}
-
-func splitHostPort(hostPort string) (string, string) {
-	s := strings.Split(hostPort, ":")
-	host := s[0]
-	if host == "" {
-		host = "0.0.0.0"
-	}
-	if len(s) == 1 {
-		return host, ""
-	}
-	return host, s[1]
+	pc.Close()
 }
 
 func mcastOpen(bindAddr net.IP, port int, ifname string) (*ipv4.PacketConn, error) {
-	s, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_DGRAM, syscall.IPPROTO_UDP)
+	// socket, create what looks like an ordinary UDP socket
+	socket, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_DGRAM, syscall.IPPROTO_UDP)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Socket:", err)
 	}
-	if err := syscall.SetsockoptInt(s, syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1); err != nil {
-		log.Fatal(err)
-	}
-	//syscall.SetsockoptInt(s, syscall.SOL_SOCKET, syscall.SO_REUSEPORT, 1)
-	if err := syscall.SetsockoptString(s, syscall.SOL_SOCKET, syscall.SO_BINDTODEVICE, ifname); err != nil {
-		log.Fatal(err)
-	}
-
-	lsa := syscall.SockaddrInet4{Port: port}
-	copy(lsa.Addr[:], bindAddr.To4())
-
-	if err := syscall.Bind(s, &lsa); err != nil {
-		syscall.Close(s)
-		log.Fatal(err)
-	}
-	f := os.NewFile(uintptr(s), "")
-	c, err := net.FilePacketConn(f)
-	f.Close()
+	// allow multiple sockets to use the same PORT number
+	err = syscall.SetsockoptInt(socket, syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("SetsockoptInt:", err)
 	}
-	p := ipv4.NewPacketConn(c)
 
-	return p, nil
+	err = syscall.SetsockoptString(socket, syscall.SOL_SOCKET, syscall.SO_BINDTODEVICE, ifname)
+	if err != nil {
+		log.Fatal("SetsockoptString:", err)
+	}
+
+	err = syscall.SetsockoptInt(socket, syscall.IPPROTO_IP, syscall.IP_MULTICAST_TTL, 64)
+	if err != nil {
+		log.Fatal("SetsockoptInt:", err)
+	}
+
+	// set up destination address
+	addr := syscall.SockaddrInet4{Port: port}
+	copy(addr.Addr[:], bindAddr.To4())
+
+	// bind
+	if err := syscall.Bind(socket, &addr); err != nil {
+		syscall.Close(socket)
+		log.Fatal("Bind:", err)
+	}
+	fd := os.NewFile(uintptr(socket), "")
+	fpc, err := net.FilePacketConn(fd)
+	fd.Close()
+	if err != nil {
+		log.Fatal("FilePacketConn:", err)
+	}
+	pc := ipv4.NewPacketConn(fpc)
+
+	return pc, nil
 }
 
-func readLoop(c *ipv4.PacketConn) {
+func readLoop(pc *ipv4.PacketConn) {
 
 	log.Printf("readLoop: reading")
 
-	buf := make([]byte, 10000)
+	buf := make([]byte, 10240)
 
 	for {
-		n, cm, src, err1 := c.ReadFrom(buf)
+		n, cm, src, err1 := pc.ReadFrom(buf)
 		if err1 != nil {
 			log.Printf("readLoop: ReadFrom: error %v", err1)
 			break
@@ -132,9 +99,9 @@ func readLoop(c *ipv4.PacketConn) {
 
 		var name string
 
-		ifi, err2 := net.InterfaceByIndex(cm.IfIndex)
-		if err2 != nil {
-			log.Printf("readLoop: unable to solve ifIndex=%d: error: %v", cm.IfIndex, err2)
+		ifi, err := net.InterfaceByIndex(cm.IfIndex)
+		if err != nil {
+			log.Printf("readLoop: unable to solve ifIndex=%d: error: %v", cm.IfIndex, err)
 		}
 
 		if ifi == nil {
@@ -145,7 +112,7 @@ func readLoop(c *ipv4.PacketConn) {
 
 		log.Printf("readLoop: recv %d bytes from %s to %s on %s", n, cm.Src, cm.Dst, name)
 
-		c.WriteTo([]byte("Hello"),cm, src)
+		pc.WriteTo([]byte("Hello"), cm, src)
 	}
 
 	log.Printf("readLoop: exiting")
