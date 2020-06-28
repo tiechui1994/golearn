32afdc4ac6a0a2d9a90a7a0b9b002e1d5f4d19c7
tiechui1994 <2904952429@qq.com>
2020-06-28 20:07:41 +0800

    add rsa encrypt

---
+++ encryption/all_test.go
@@ -0,0 +1,23 @@
+package encryption
+
+import (
+	"testing"
+	"encoding/base64"
+)
+
+func TestRSA(t *testing.T) {
+	origin := []byte("Hello World")
+	cipher, err := RSAEncrpty(origin)
+	if err != nil {
+		t.Fatal("RSAEncrpty", err)
+	}
+
+	t.Log("cipher", base64.StdEncoding.EncodeToString(cipher))
+
+	plain, err := RSADecrpty(cipher)
+	if err != nil {
+		t.Fatal("RSADecrpty", err)
+	}
+
+	t.Log("plain", string(plain))
+}
---
+++ encryption/rsa.go
@@ -0,0 +1,59 @@
+package encryption
+
+import (
+	"io/ioutil"
+	"encoding/pem"
+	"fmt"
+	"crypto/x509"
+	"crypto/rsa"
+	"crypto/rand"
+)
+
+var (
+	pub_key string
+	pri_key string
+)
+
+func RSAEncrpty(msg []byte) (cipher []byte, err error) {
+	if pub_key == "" {
+		data, _ := ioutil.ReadFile("./rsa/rsa_public_key.pem")
+		pub_key = string(data)
+	}
+
+	// resolve pem format public key
+	block, _ := pem.Decode([]byte(pub_key))
+	if block == nil {
+		return nil, fmt.Errorf("public key error")
+	}
+
+	// resolve public key
+	pubkey, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	pub := pubkey.(*rsa.PublicKey)
+
+	return rsa.EncryptPKCS1v15(rand.Reader, pub, msg)
+}
+
+func RSADecrpty(msg []byte) (plain []byte, err error) {
+	if pri_key == "" {
+		data, _ := ioutil.ReadFile("./rsa/rsa_private_key.pem")
+		pri_key = string(data)
+	}
+
+	// resolve pem format private key
+	block, _ := pem.Decode([]byte(pri_key))
+	if block == nil {
+		return nil, fmt.Errorf("private key error")
+	}
+
+	// resolve private key
+	privkey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return rsa.DecryptPKCS1v15(rand.Reader, privkey, msg)
+}
