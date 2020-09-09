1129d050aca6a3efcc3124fa997d6a95c7820e78
tiechui1994 <2904952429@qq.com>
2020-09-09 13:22:16 +0800

    move file

--- detail/array_init.go
+++ detail/array_init.go
@@ -1,25 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-// 字面量在初始化切片的时候,可以指定索引, 没有指定的索引的元素会在前一个索引基础上加1
-// 如果切边当中,开始位置没有指定索引, 则默认是0. 切片当中的索引不能重复(隐式和显式)
+/**
+字面量在初始化切片的时候, 可以显示指定索引, 没有指定的索引的元素会在前一个索引基础上加1. 例如 {"x","y"}, 那么针对 "y"
+的索引就是1. {1:"x", "y"}, 那么此时 "y" 的索引就是2.
+
+如果切片当中, 开始位置没有指定索引, 则默认是0. 切片当中的索引不能重复(隐式推断和显式指定).
+
+索引推断的规则是从左到右, 如果有显示指定, 则使用显示指定, 否则使用隐式推断. 注意: 在这个过程中任何一个位置的索引不能重复.
+**/
 func ArrayInit() {
 	var x = []int{2: 3, 2, 0: 1}
 	fmt.Println(x) // [1 0 2 3]
 
-	//var _ = []int{1, 2, 1: 3} // 编译失败,duplicate index in array literal: 0
+	//var _ = []int{1, 2, 1: 3} // 编译失败, duplicate index in array literal: 0
 	//var _ = []int{1, 2, 2: 3} // 编译成功
 }
 
-func MapAndPointer() {
-	type Param map[string]interface{}
+/**
+如果 map 的值是指针类型, 那么每次获取 map 的值的时候都是获得一个指向底层值的指针, 在指针的基础上可以直接修改值.
 
-	type Show struct {
-		*Param
+如果 map 的值是非指针类型, 每次获取 map 的值的时候都会将原来的值复制一份, 这样的在修改的时候, 只能是修改其原有值的副本,
+一定要记得将修改后的值进行进行重新写入.
+**/
+func Value() {
+	type T struct {
+		Val int
 	}
 
-	// s := new(Show)    // 只是创建一个Show的指针, 实例的*Param为nil, [只有通过make创建map,然后引用赋值有效]
-	// s.Param["A"] = 100 // 指针不支持引用, s.Param是一个指针, 无法进行引用
-	// (*s.Param)["A"] = 100 // 合法
+	m1 := map[string]T{}
+	m1["A"] = T{Val: 100}
+	fmt.Printf("before:%+v\n", m1["A"])
+	// m1["A"].Val = 200 // cannot assign to struct field m1["A"].Val in map
+	val := m1["A"]
+	val.Val = 300 // no uesed
+	fmt.Printf("after:%+v\n", m1["A"])
+
+	m2 := map[string]*T{}
+	m2["B"] = &T{Val: 100}
+	fmt.Printf("before:%+v\n", m2["B"])
+	m2["B"].Val = 200
+	fmt.Printf("after: %+v\n", m2["B"])
+
+	s1 := make([]T, 2)
+	s1[0] = T{Val: 20}
+	fmt.Printf("before:%+v\n", s1[0])
+	s1[0].Val = 200
+	fmt.Printf("before:%+v\n", s1[0])
+
+}
+
+func main() {
+	Value()
 }
---
+++ detail/go.md
@@ -0,0 +1,69 @@
+## go 命令
+
+
+### go build
+
+常用的参数:
+
+- `-a` 用于强制重新编译所有涉及的 Go 语言代码包(包括Go语言标准库的代码包), 即使它们已经是最新的了. 该标记可以让我们有
+机会通过改动底层的代码包做一些实验.
+
+- `-n` 仅打印执行过程中用到的所有命令, 而不去真正执行命令.
+
+- `-x` 打印命令执行过程中用到的所有命令, 并同时执行命令.
+
+- `-race` 用于检测并报告指定 Go 语言程序存在的数据竞争问题. 当用 Go 语言编写并发程序的时候, 这是很重要的检测手段.
+
+- `-v` 打印命令执行过程中涉及的代码包. 
+
+- `-work` 打印命令执行时使用的临时工作目录的名字, 且命令执行完成后不删除它. 这个目录下的文件可以从侧面了解命令的执行过
+程. 如果没有此标记, 那么临时目录会在命令执行完毕前删除.
+
+- `-ldflags '[pattern=]arg list'` 用于传递每个 `go tool link` 调用的参数.
+
+```
+-I linker  添加搜索header文件的目录
+-L directory 将指定目录添加到库路径
+
+-c        dump call graph
+-n        dump symbol table
+-dumpdep  dump symbol dependency graph
+
+-buildid id 设置编译唯一标识
+-buildmode mode 设置编译mode
+-linkmode mode 设置链接mode
+
+-extar string  存档程序, buildmode=c-archive
+-extld linker  在external mode下链接时使用的链接器.
+-extldflasgs flags 将链接参数传递给外部链接器
+
+-o file 将输出写入到指定文件
+
+-race 启用竞态检测
+
+-s 禁止生成符号表(symbol table)
+-w 禁止生成 DWARF 
+-v 打印link的追踪
+```
+
+- `-gcflags '[pattern=]arg list'` 用于传递每个 `go tool compile` 调用的参数.
+
+```
+-B 禁止边界检测
+-C 禁止打印错误消息的行
+
+-D path 设置本地导入的相对路径.
+-I directory 将directory添加到导入搜索的目录
+
+-N 禁止编译器优化
+-l 禁止内联
+
+-S 打印汇编(assembly)结果
+-V 打印版本并退出
+
+-W debug parse tree after type checking
+-w debug type checking
+
+-asmhdr file 将汇编的header写入到文件file
+```
+
