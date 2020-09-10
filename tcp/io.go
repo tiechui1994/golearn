3d1cbb95239a020ac629d552802eba000a9e93b5
tiechui1994 <2904952429@qq.com>
2020-09-10 20:23:09 +0800

    add tcp io pipe read

--- tcp/io.go
+++ tcp/io.go
@@ -1,1 +1,49 @@
-package tcp
+package main
+
+import (
+	"log"
+	"io"
+	"encoding/json"
+	"net/http"
+)
+
+func init() {
+	log.SetFlags(log.Lshortfile)
+}
+
+func post() {
+	msg := struct {
+		Name, Addr string
+		Price      float64
+	}{
+		Name:  "hello",
+		Addr:  "beijing",
+		Price: 123.12,
+	}
+
+	r, w := io.Pipe()
+	go func() {
+		defer w.Close()
+		err := json.NewEncoder(w).Encode(msg)
+		if err != nil {
+			log.Println("encode", err)
+		}
+		log.Println("encode success")
+	}()
+
+	response, err := http.DefaultClient.Post("https://www.baidu.com", "application/json", r)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("hello")
+
+	body := response.Body
+	defer body.Close()
+	log.Println("job success")
+}
+
+
+func main() {
+	post()
+}
