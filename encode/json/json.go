1222d933af4790c13b5aa83f2fd95956436aa52f
tiechui1994 <2904952429@qq.com>
2020-12-12 18:12:20 +0800

    add new content

--- encode/json/json.go
+++ encode/json/json.go
@@ -3,13 +3,15 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"bytes"
 )
 
 type Student struct {
 	Child []int `json:"child"`
 }
 
-func main() {
+func example() {
 	data := `{"child":[1,2,3]}`
 	var a Student
 	json.Unmarshal([]byte(data), &a)
@@ -19,4 +21,40 @@ func main() {
 	data = `{"child":[3,4,5,6,7,8]}`
 	json.Unmarshal([]byte(data), &a)
 	fmt.Println(aa)
+}
+
+func valid() {
+	// json 合法的开始头部
+	case1 := "123"
+	fmt.Println(case1, json.Valid([]byte(case1)))
+
+	case2 := "0.123"
+	fmt.Println(case2, json.Valid([]byte(case2)))
+
+	case3 := "-0"
+	fmt.Println(case3, json.Valid([]byte(case3)))
+
+	case4 := "true"
+	fmt.Println(case4, json.Valid([]byte(case4)))
+
+	case5 := "false"
+	fmt.Println(case5, json.Valid([]byte(case5)))
+
+	case6 := "{}"
+	fmt.Println(case6, json.Valid([]byte(case6)))
+
+	case7 := "[]"
+	fmt.Println(case7, json.Valid([]byte(case7)))
+
+	case8 := `""`
+	fmt.Println(case8, json.Valid([]byte(case8)))
+}
+
+func write() {
+	data := make([]byte, 8192)
+	copy(data[0:4096], bytes.Repeat([]byte{'·'}, 4096))
+	ioutil.WriteFile("./www.data", data, 066)
+}
+func main() {
+	write()
 }
--- encode/json/json.md
+++ encode/json/json.md
@@ -21,3 +21,49 @@ func main() {
 }
 ```
 
+## Unmarshal 
+
+`Unmarshal` 方法是 JSON 解析, 将一个JSON字符串转换成为一个对象. 这个方法的过程很简单, 就两步操作, 第一步是检查 JSON 
+字符串的合法性, 第二步是将 JSON 字符串解析成为一个`指针对象`.
+
+```cgo
+func Unmarshal(data []byte, v interface{}) error {
+	var d decodeState
+	err := checkValid(data, &d.scan) // 检查 JSON 合法性
+	if err != nil {
+		return err
+	}
+
+	d.init(data)
+	return d.unmarshal(v) // 解析
+}
+```
+
+检查 JSON 字符串的合法性, 其实在 `json` 包当中专门提供了一个方法 `json.Valid()`. 方法的原理是 `栈` + 状态迁移函数.
+状态迁移函数转换图如下:
+
+![image](/images/encode_json_valid.png)
+
+
+
+接下来详细的介绍一下 `unmarshal` 方法.
+
+```cgo
+func (d *decodeState) unmarshal(v interface{}) error {
+    // 检查 v, 必须是一个非空指针
+	rv := reflect.ValueOf(v)
+	if rv.Kind() != reflect.Ptr || rv.IsNil() {
+		return &InvalidUnmarshalError{reflect.TypeOf(v)}
+	}
+
+	d.scan.reset() // 初始化 scanner 的状态迁移函数 `stateBeginValue`.
+	d.scanWhile(scanSkipSpace) // scanSkipSpace 是扫描的一个标记, 表示可以忽略当前的空格字符
+	// 解码rv而不是rv.Elem, 因为必须在值的顶层应用Unmarshaler接口测试.
+	err := d.value(rv)
+	if err != nil {
+		return d.addErrorContext(err)
+	}
+	return d.savedError
+}
+```
+
