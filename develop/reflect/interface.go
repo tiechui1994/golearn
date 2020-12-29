1d56f4feb14406a6e3c4eb4ae9e678a611aff02d
tiechui1994 <2904951429@qq.com>
2020-12-29 21:11:47 +0800

    update attrs

--- develop/reflect/interface.go
+++ develop/reflect/interface.go
@@ -1,13 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"bytes"
+	"time"
+	"math/rand"
+)
 
 type Human interface {
 	Say() string
 }
 
-type Man struct {
-}
+type Man struct{}
 
 func (m *Man) Say() string {
 	return "man"
@@ -27,7 +32,7 @@ func IsNil(h interface{}) bool {
 // interface, iface(itab为nil + data为nil) eface(_type为nil + data为nil)
 // pointer, channel, slice, map, func 只是声明未初始化
 
-func main() {
+func NilTestCase() {
 	var a1 [10]int
 	var b1 [10]int
 	fmt.Println(a1 == b1)
@@ -108,4 +113,75 @@ func main() {
 	var h1 Human = &m1
 	var h2 interface{} = m2
 	fmt.Println(h1 == h2)
+}
+
+//==================================================================================================
+
+type Person interface {
+	grow()
+}
+
+type Student struct {
+	Age  int    `json:"age"`
+	Name string `json:"name"`
+}
+
+func (p Student) grow() {
+	p.Age += 1
+	return
+}
+
+func ConvertCase() {
+	// 如果接口的类型一致, 那么接口转换是不产生额外的操作的
+	// var x Interface
+	// _ = Interface(x)
+	// 其中 Interface 是 iface
+	// 接口转换
+	v1 := Person(Student{Age: 108, Name: "abc"}) // runtime.convT2I
+	val := io.ByteScanner(&bytes.Buffer{})
+	v6 := io.ByteReader(val) // runtime.convI2I
+	fmt.Println(v1, v6)
+
+	// 如果接口的值类型和断言的类型一致, 则不会产生额外的操作
+	// var x Interface = T{}
+	// _, _ = x.(T)
+	// 其中 Interface 可以是 eface, 也可以是iface
+	// 接口断言
+	var eface interface{} = Student{}
+	v2, _ := eface.(Person) // runtime.assertE2I2
+	v3 := eface.(Person)    // runtime.assertE2I
+
+	var iface Person = Student{} // runtime.convT2E
+	v4, _ := iface.(Person)      // runtime.assertI2I2
+	v5 := iface.(Person)         // runtime.assertI2I
+
+	fmt.Println(v2, v3, v4, v5)
+}
+
+//==================================================================================================
+
+// hash - 二次寻址
+
+func hash() {
+	rand.Seed(time.Now().UnixNano())
+	for k := uintptr(1); k < 68; k++ {
+		var mask uintptr = 1<<k - 1
+		var h0 = uintptr(rand.Uint64())
+
+		h := h0
+		for i := uintptr(1); ; i++ {
+			h += i
+			h &= mask
+
+			fmt.Println(h, (h0+i*(i+1)/2)&mask)
+
+			if i >= 100 {
+				break
+			}
+		}
+	}
+}
+
+func main() {
+
 }
--- tcp/net/net_test.go
+++ tcp/net/net_test.go
@@ -3,6 +3,7 @@ package net
 import (
 	"fmt"
 	"testing"
+	"unsafe"
 )
 
 func TestListen(t *testing.T) {
@@ -21,4 +22,43 @@ func IsNil(addr Addr) {
 
 func TestNil(t *testing.T) {
 	IsNil(nil)
+}
+
+func TestPollDesc(t *testing.T) {
+	type timer struct {
+		tb     uintptr // the bucket the timer lives in
+		i      int     // heap index
+		when   int64
+		period int64
+		f      func(interface{}, uintptr)
+		arg    interface{}
+		seq    uintptr
+	}
+
+	type mutex struct {
+		key uintptr
+	}
+
+	type pollDesc struct {
+		link *pollDesc // in pollcache, protected by pollcache.lock
+
+		lock    mutex // protects the following fields
+		fd      uintptr
+		closing bool
+		everr   bool    // marks event scanning error happened
+		user    uint32  // user settable cookie
+		rseq    uintptr // protects from stale read timers
+		rg      uintptr // pdReady, pdWait, G waiting for read or nil
+		rt      timer   // read deadline timer (set if rt.f != nil)
+		rd      int64   // read deadline
+		wseq    uintptr // protects from stale write timers
+		wg      uintptr // pdReady, pdWait, G waiting for write or nil
+		wt      timer   // write deadline timer
+		wd      int64   // write deadline
+	}
+
+	t.Log("pollDesc Size", unsafe.Sizeof(pollDesc{}))
+
+	t.Log("timer Size", unsafe.Sizeof(timer{}))
+	t.Log("timer Size", unsafe.Sizeof(timer{}.arg))
 }
