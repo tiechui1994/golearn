22c20b38743b53392ca0c842fc267e1d610ba2ad
tiechui1994 <2904951429@qq.com>
2021-02-19 23:23:00 +0800

    add new

--- algorithm/category/dynamic-programming.go
+++ algorithm/category/dynamic-programming.go
@@ -7,11 +7,11 @@ package category
 /*
 292: 捡石子游戏 (一次可以最多三个石子, 拿到最后一个的赢)
 
-递推公式
+递推公式:
 
 dp[i] 表示 i 个石子, 结果
 
-dp[i] = !(dp[i-1] || dp[i-2] || dp[i-3])
+dp[i] = !( dp[i-1] || dp[i-2] || dp[i-3] )
 
 第 i 个石子, 取决于第 i-1, i-2, i-3 的状态(对手的状态), 如果任何一个对手状态都是true, 那么你必输.
 */
@@ -53,50 +53,10 @@ func CanIWin(n int) bool {
 }
 
 /*
-486 : 一个数组, 每次只能从数组的左边或者右边拿数, 最终拿到数字之和最大的赢. 如果你先拿, 是否能赢?
-*/
-
-func PredictTheWinner(arr []int) bool {
-	const Max = int(1>>64 - 1)
-
-	/**
-	dfs 的作用就是计算 i, j 之间最大差距
-	i, j 表示数组的索引范围
-	memo[i][j] 表示在范围 i..j 之间最大结果最大差距(我和对手), 那么 memo[0][n-1] 则是结果
-
-	memo[i][j] = max( arr[i] - memo[i+1][j], arr[j] - memo[i][j-1] )
-	*/
-	var dfs func(arr []int, i, j int, memo [][]int) int
-	dfs = func(arr []int, i, j int, memo [][]int) int {
-		if i > j {
-			return 0
-		}
-
-		if memo[i][j] != Max {
-			return memo[i][j]
-		}
-
-		memo[i][j] = max(arr[i]-dfs(arr, i+1, j, memo),
-			arr[j]-dfs(arr, i, j-1, memo))
-		return memo[i][j]
-	}
-
-	n := len(arr)
-	var memo [][]int
-	for i := 0; i < n; i++ {
-		item := make([]int, n)
-		for j := 0; j < n; j++ {
-			item[j] = Max
-		}
-		memo = append(memo, item)
-	}
-
-	return dfs(arr, 0, n-1, memo) >= 0
-}
-
-/*
 1025: 最初有一个数字 N, 然后选择一个数字 x, 数字 x 满足的要求: 0 < x < N, 并且 N % x == 0,
 接着使用 N-x 代替原来的 N, 直到玩家无法执行上述的操作. 能拿到最后一个数字的玩家获胜.
+
+dp[i] = !( dp[k] || dp[k] )   k 满足的条件是 N % x == 0
 */
 
 func DivisorGame(n int) bool {
@@ -134,6 +94,58 @@ func DivisorGame(n int) bool {
 }
 
 /*
+486: 一个数组, 每次只能从数组的左边或者右边拿数, 最终拿到数字之和最大的赢. 如果你先拿, 是否能赢?
+
+memo[i][j] 表示在范围 i..j 之间双方最大分数差, 那么 memo[0][n-1] 则是结果.
+
+注: 只有当差值最大时(先手-后手), 才能判断先手在最好的状况下是否能赢.
+
+memo[i][j] = max( arr[i] - memo[i+1][j], arr[j] - memo[i][j-1] )
+*/
+
+func StoneGameI(piles []int) bool {
+	const Max = int(1>>64 - 1)
+
+	/**
+	dfs 的作用就是计算 i, j 之间最大差距
+	i, j 表示数组的索引范围
+	memo[i][j] 表示在范围 i..j 之间双方最大分数差, 那么 memo[0][n-1] 则是结果
+
+	memo[i][j] = max( arr[i] - memo[i+1][j], arr[j] - memo[i][j-1] )
+	*/
+	var dfs func(arr []int, i, j int, memo [][]int) int
+	dfs = func(arr []int, i, j int, memo [][]int) int {
+		if i > j {
+			return 0
+		}
+
+		if memo[i][j] != Max {
+			return memo[i][j]
+		}
+
+		// 分别表示先手拿第一个和最后一个能的结果
+		memo[i][j] = max(
+			arr[i]-dfs(arr, i+1, j, memo),
+			arr[j]-dfs(arr, i, j-1, memo),
+		)
+
+		return memo[i][j]
+	}
+
+	n := len(piles)
+	var memo [][]int
+	for i := 0; i < n; i++ {
+		item := make([]int, n)
+		for j := 0; j < n; j++ {
+			item[j] = Max
+		}
+		memo = append(memo, item)
+	}
+
+	return dfs(piles, 0, n-1, memo) >= 0
+}
+
+/*
 1140: 石子游戏II
 
 给定一个数字 piles, piles[i] 表示第 i 堆石子的个数.
@@ -159,31 +171,30 @@ func StoneGameII(piles []int) int {
 	游戏增加了一个限制: 每次取的数据可是是前一次的2倍, 因此当次取的最优策略是限制下一次取的值的最小, 那么这次获取就是最
 	大了.
 	*/
-	var dfs func(arr []int, index, M int, memo [][]int, sum []int) int
-	dfs = func(arr []int, index, M int, memo [][]int, sum []int) int {
+	var dfs func(arr []int, i, M int, memo [][]int, sum []int) int
+	dfs = func(arr []int, i, M int, memo [][]int, sum []int) int {
 		// 边界
-		if index == len(arr) {
+		if i == len(arr) {
 			return 0
 		}
 		// 剩余石子不足2M
-		if len(arr)-index <= 2*M {
-			return sum[index]
+		if len(arr)-i <= 2*M {
+			return sum[i]
 		}
 
-		if memo[index][M] != 0 {
-			return memo[index][M]
+		if memo[i][M] != 0 {
+			return memo[i][M]
 		}
 
 		// 限制对手拿到最小的值
 		score := int(1>>64 - 1)
 		for i := 1; i <= 2*M; i++ {
-			score = min(score,
-				dfs(arr, index+1, max(M, i), memo, sum))
+			score = min(score, dfs(arr, i+1, max(M, i), memo, sum))
 		}
 
-		memo[index][M] = sum[index] - score
+		memo[i][M] = sum[i] - score
 
-		return sum[index] - score
+		return memo[i][M]
 	}
 
 	// memo[i][j] 表示在 [i:N] 堆时, M=j的状况下, 先取的人能获得的最多石子数量
@@ -206,7 +217,7 @@ func StoneGameII(piles []int) int {
 A, B 玩这个游戏, 谁会赢(A是先手, 并且A,B都采用最优策略)?
 */
 
-func StoneGameIII(piles []int) int {
+func StoneGameIII(piles []int) string {
 	const Max = int(1>>64 - 1)
 
 	N := len(piles)
@@ -232,7 +243,7 @@ func StoneGameIII(piles []int) int {
 		}
 
 		score := 0
-		res := Max
+		res := -Max
 		for k := i; k < i+3 && k < len(arr); k++ {
 			score += arr[k]
 			res = max(res, score-dfs(arr, k+1, memo))
@@ -248,5 +259,14 @@ func StoneGameIII(piles []int) int {
 		memo[i] = Max
 	}
 
-	return dfs(piles, 0, memo)
+	ans := dfs(piles, 0, memo)
+
+	if ans == 0 {
+		return "E"
+	}
+	if ans > 0 {
+		return "A"
+	}
+	
+	return "B"
 }
