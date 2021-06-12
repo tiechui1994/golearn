735d3b30935900891a64ddb74318548dda1ccfdb
tiechui1994 <2904951429@qq.com>
2021-06-12 20:55:35 +0800

    update idea

--- tool/idea.go
+++ tool/idea.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
 	"flag"
 	"io/ioutil"
 	"log"
+	"net/url"
 	"os"
 	"path/filepath"
 	"regexp"
 	"strings"
+	"time"
 )
 
 // http://idea.94goo.com/
 // http://idea.do198.com/
+// http://vrg123.com/loadcode/ 4565
 
 // go build -o idea -ldflags '-w -s' idea.go
 
@@ -52,7 +58,7 @@ func writeCode(code, file string) {
 	log.Printf("Success write %v", file)
 }
 
-func getCode(u string) string {
+func getCode1(u string) string {
 	data, err := GET(u, nil)
 	if err != nil {
 		log.Println(err)
@@ -66,6 +72,76 @@ func getCode(u string) string {
 	}
 
 	return ""
+}
+
+func getCode2() string {
+	data, err := GET("http://vrg123.com/", nil)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+
+	str := strings.Replace(string(data),"\n","",-1)
+	var token string
+	re := regexp.MustCompile(`<input type="hidden" name="csrfmiddlewaretoken" value="(.*?)">`)
+	tokens := re.FindAllStringSubmatch(str, 1)
+	if len(tokens) == 1 && len(tokens[0]) == 2 {
+		token = tokens[0][1]
+	}
+
+	if token == "" {
+		return ""
+	}
+
+	val := url.Values{}
+	val.Set("password", "4565")
+	header := map[string]string{
+		"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+		"Cookie":       "csrftoken=" + token,
+		"X-CSRFToken":  token,
+	}
+	u := "http://vrg123.com/loadcode/"
+	data, err = POST(u, bytes.NewBufferString(val.Encode()), header)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+
+	var result struct {
+		Code    int    `json:"code"`
+		Codeval string `json:"codeval"`
+	}
+	json.Unmarshal(data, &result)
+	return result.Codeval
+}
+
+func validCode(code string) bool {
+	tokens := strings.Split(code, "-")
+	if len(tokens) < 2 {
+		return false
+	}
+
+	var meta struct {
+		LicenseId string `json:"licenseId"`
+		Products  []struct {
+			Code     string `json:"code"`
+			PaidUpTo string `json:"paidUpTo"`
+			Extended bool   `json:"extended"`
+		} `json:"products"`
+	}
+
+	data, err := base64.StdEncoding.DecodeString(tokens[1])
+	if err != nil {
+		return false
+	}
+
+	err = json.Unmarshal(data, &meta)
+	if err != nil {
+		return false
+	}
+
+	return meta.LicenseId == tokens[0] && len(meta.Products) > 0 &&
+		meta.Products[0].PaidUpTo >= time.Now().Format("2006-01-02")
 }
 
 func searchFile(dir string) []string {
@@ -95,11 +171,15 @@ func main() {
 	dir := flag.String("path", "/root", "jetbrains work dir")
 	flag.Parse()
 
-	code := getCode("http://idea.94goo.com")
-	if code == "" {
+	code := getCode1("http://idea.94goo.com")
+	if !validCode(code) {
+		code = getCode2()
+	}
+	if !validCode(code) {
+		log.Println("no code")
 		return
 	}
-
+	
 	paths := searchFile(*dir)
 	for _, path := range paths {
 		writeCode(code, path)
